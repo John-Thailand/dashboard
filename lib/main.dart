2937e17c89fda566cd7b5535d007eb50e4f18623
John-Thailand <hamoban69@gmail.com>
2022-03-22 20:22:27 +0900

    レスポンシブ対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
-import 'package:dashboard/screens/home.dart';
+import 'package:dashboard/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: "Dash",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // ScaffoldWidgetの背景色です。
+        scaffoldBackgroundColor: Colors.white,
+        // Text Widgetの基本的なスタイルです。
+        // Applies the Mulish font family from Google Fonts to every [TextStyle] in the given [textTheme].
+        textTheme:
+            GoogleFonts.mulishTextTheme(Theme.of(context).textTheme).apply(
+          bodyColor: Colors.black,
+        ),
+        // 画面遷移にどのようなアニメーションを使うかというThemeです。
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            // FadeUpwardsPageTransitionsBuilder：Android向け標準トランジション
+            TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          },
+        ),
+        // アプリの基本色になります。
+        primaryColor: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: SiteLayout(),
     );
   }
 }
---
+++ lib/widgets/small_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SmallScreen extends StatelessWidget {
+  const SmallScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Creates box constraints that expand to fill another box constraints.
+      //// If width or height is given, the constraints will require exactly the given value in the given dimension.
+      constraints: BoxConstraints.expand(),
+      color: Colors.green,
+    );
+  }
+}
