5b7a1c026e743affc7aabf57ab8fc8ff93104c4a
John-Thailand <hamoban69@gmail.com>
2022-03-24 10:22:43 +0900

    ドロワーメニューの追加

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -8,38 +8,61 @@ class SideMenu extends StatelessWidget {
     return ListView(
       children: <Widget>[
         DrawerHeader(
-          child: Text('ヘッダー'),
-          decoration: BoxDecoration(
-            color: Colors.blue,
+          child: Column(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text("User Name"),
+                accountEmail: Text("User Email"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(
+                      "https://pbs.twimg.com/profile_images/885510796691689473/rR9aWvBQ_400x400.jpg"),
+                ),
+              ),
+            ],
           ),
         ),
         ListTile(
-          title: Text("ボタン"),
-          trailing: Icon(Icons.arrow_forward),
+          title: const Text(
+            "Introduction",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+          leading: const Icon(Icons.person),
+          onTap: () {
+            // 自己紹介ページに遷移
+          },
         ),
         ListTile(
-          title: Text("ボタン"),
-          trailing: Icon(Icons.arrow_forward),
+          title: const Text(
+            "Blog",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+          leading: const Icon(Icons.chat),
+          onTap: () {
+            // ブログページに遷移
+          },
         ),
         ListTile(
-          title: Text("ボタン"),
-          trailing: Icon(Icons.arrow_forward),
-        ),
-        ListTile(
-          title: Text("ボタン"),
-          trailing: Icon(Icons.arrow_forward),
-        ),
-        ListTile(
-          title: Text("ボタン"),
-          trailing: Icon(Icons.arrow_forward),
-        ),
-        ListTile(
-          title: Text("ボタン"),
-          trailing: Icon(Icons.arrow_forward),
-        ),
-        ListTile(
-          title: Text("ボタン"),
-          trailing: Icon(Icons.arrow_forward),
+          title: const Text(
+            "Contact",
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+          leading: const Icon(Icons.contact_mail),
+          onTap: () {
+            // お問い合わせページに遷移
+          },
         ),
       ],
     );
