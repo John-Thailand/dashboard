d1cdebca8b2881d91490a1a6e78c04229bfda6c2
John-Thailand <hamoban69@gmail.com>
2022-03-26 14:04:43 +0900

    Horizontal Side Menu 切り分け

---
+++ lib/components/widgets/horizontai_side_menu.dart
@@ -0,0 +1,97 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dashboard/view/style.dart';
+import 'package:dashboard/view_model/user_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HorizontalSideMenu extends HookConsumerWidget {
+  const HorizontalSideMenu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ユーザーの状態を管理する
+    final userState = ref.watch(userViewModel.notifier).state;
+
+    return ListView(
+      children: <Widget>[
+        // Solution: ContainerによってDrawerHeaderの高さを設定
+        Container(
+          height: 216,
+          color: Colors.blue,
+          child: DrawerHeader(
+            child: UserAccountsDrawerHeader(
+              accountName: AutoSizeText(
+                userState.name,
+                presetFontSizes: const [16, 15, 14, 13, 12, 11, 10],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              accountEmail: AutoSizeText(
+                userState.email,
+                presetFontSizes: const [14, 13, 12, 11, 10],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage(userState.imageUrl),
+              ),
+            ),
+          ),
+        ),
+        ListTile(
+          title: const AutoSizeText(
+            "Introduction",
+            presetFontSizes: [16, 15, 14, 13, 12, 11, 10],
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyles.sideMenuItemTextStyle,
+          ),
+          leading: const Icon(Icons.person),
+          onTap: () {
+            // 自己紹介ページに遷移
+          },
+        ),
+        ListTile(
+          title: const AutoSizeText(
+            "Blog",
+            presetFontSizes: [16, 15, 14, 13, 12, 11, 10],
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyles.sideMenuItemTextStyle,
+          ),
+          leading: const Icon(Icons.chat),
+          onTap: () {
+            // ブログページに遷移
+          },
+        ),
+        ListTile(
+          title: const AutoSizeText(
+            "Contact",
+            presetFontSizes: [16, 15, 14, 13, 12, 11, 10],
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyles.sideMenuItemTextStyle,
+          ),
+          leading: const Icon(Icons.contact_mail),
+          onTap: () {
+            // お問い合わせページに遷移
+          },
+        ),
+        ListTile(
+          title: const AutoSizeText(
+            "Contact",
+            presetFontSizes: [16, 15, 14, 13, 12, 11, 10],
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyles.sideMenuItemTextStyle,
+          ),
+          leading: const Icon(Icons.contact_mail),
+          onTap: () {
+            // お問い合わせページに遷移
+          },
+        ),
+      ],
+    );
+  }
+}
